dcc72f6b0c80a0e0cc2236db9e898f5bb6e70715
Creature <zuzanna.ewa.klaus@gmail.com>
2018-09-19 23:10:58 +0200

    initial commit

---
+++ CreaturelingAI/src/main.ts
@@ -0,0 +1,22 @@
+import { profiler } from "./external/screeps-profiler";
+
+function errGuard(foo: () => void)
+  {
+  try
+    {
+    foo();
+    }
+  catch(err)
+    {
+    console.log('<font color="#ff3333" type="highlight">' + err.stack + '</font>');
+    }
+  }
+
+profiler.enable();
+
+module.exports.loop = function() {
+  profiler.wrap(function()
+  {
+  console.log(Game.time);
+  });
+  }
